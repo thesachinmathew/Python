22f3d4218bd20fbf87900badb624ce18d96bbf85
Sachin Mathew <167275639+thesachinmathew@users.noreply.github.com>
2024-08-22 13:11:00 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -1,18 +1,8 @@
 #find the freq of each character of a string and replace each character by the aplhabet which is at distance = freq from the character
 def rep(s: str) -> str:
-    f = {}
-    for ch in s:
-        if ch in f:
-            f[ch] += 1
-        else:
-            f[ch] = 1
-    res = []
-    for ch in s:
-        freq = f[ch]
-        nw = chr((ord(ch) - ord('a') + freq) % 26 + ord('a'))
-        res.append(nw)
-    return ''.join(res)
+    return ''.join(
+        chr((ord(ch) - ord('a') + (s.count(ch))) % 26 + ord('a')) 
+        for ch in s
+    )
 if __name__ == "__main__":
-    inn = input("Enter the string: ")
-    op = rep(inn)
-    print("Resulting string:", op)
+    print("Resulting string:", rep(input("Enter the string: ")))
