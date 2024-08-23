818419bb2358d558d7f9f7c7182625c716fbbd37
Sachin Mathew <167275639+thesachinmathew@users.noreply.github.com>
2024-08-23 08:55:24 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -16,12 +16,24 @@ def a(year, month):
     return wec, fmon
 if __name__ == "__main__":
     year = int(input())
-    month = int(input())
+    month = int(input())                                                                                                                                                  
     we, mon = a(year, month)
     print(f"Number of weekdays: {we}\nDate of the first Monday: {mon.strftime('%Y-%m-%d')}")
 
 
 #get string input add ^ if a character is vowels and add @ after character if not vowel 
 print(''.join(c + '^' if c in 'aeiouAEIOU' else c + '@' for c in input()))
+
+#avg students in library
+en= [40, 20, 10, 30, 20]
+le= [20, 10, 20, 10, 15]
+def avg(en, le):
+    cs = 0
+    ts = 0
+    for e, l in zip(en, le):
+        cs += e - l
+        ts += cs
+    return ts / len(en)
+print("Average number of students in the library:", avg(en, le))
 
 
