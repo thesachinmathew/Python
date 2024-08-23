690a681f72694874a852cd6d3c00626852a7cc8c
Sachin Mathew <167275639+thesachinmathew@users.noreply.github.com>
2024-08-23 09:05:21 +0530

    Update day10.py

--- day10.py
+++ day10.py
@@ -36,4 +36,26 @@ def avg(en, le):
     return ts / len(en)
 print("Average number of students in the library:", avg(en, le))
 
+#using dictionary
+data = {
+    1: {'entering': 40, 'leaving': 20},
+    2: {'entering': 20, 'leaving': 10},
+    3: {'entering': 10, 'leaving': 20},
+    4: {'entering': 30, 'leaving': 10},
+    5: {'entering': 20, 'leaving': 15}
+}
+def average_students(data):
+    current_students = 0
+    total_students = 0
+    hours = len(data)
+    
+    for hour in range(1, hours + 1):
+        e = data[hour]['entering']
+        l = data[hour]['leaving']
+        current_students += e - l
+        total_students += current_students
+        print(f"Hour {hour}: Entering={e}, Leaving={l}, Current Students={current_students}")
+    
+    return total_students / hours
 
+print("Average number of students in the library:", average_students(data))
