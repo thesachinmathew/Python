82a797d900e64a4df3d4853ef912b0d6c3895dc9
Sachin Mathew <167275639+thesachinmathew@users.noreply.github.com>
2024-08-24 12:50:52 +0530

    Create day11.py

---
+++ day11.py
@@ -0,0 +1,5 @@
+#find the common elements in three sorted array
+#find the length of the longet increasing subsequents in the given array
+#find the majority elements in an array the apears more than n/2 times
+#wap to find the min num of jumps to reach the end of an array where each elm is the min jump length
+#longest common sudseqeunts
